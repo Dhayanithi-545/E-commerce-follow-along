7e1310ec70de002a925b80240c2dd968eb25118b
Dhayanithi <545nithi@gmail.com>
2025-02-06 14:02:42 +0530

    Updated

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+
+import './App.css';
+import React from 'react';
+import {Home, Login, Signup} from "./Routes/routes"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/login' element={<Login/>}/>
+    <Route path='/signup' element={<Signup/>}/>
+    </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ frontend/src/Routes/routes.jsx
@@ -0,0 +1,4 @@
+import Login from "../pages/Login.jsx";
+import Signup from "../pages/Signup.jsx";
+import Home from "../pages/Home.jsx";
+export {Login,Signup,Home}
